3d7f6a403f2945731dd6b0250f210f3a40cdebcf
Vishal-RAJ-DEV <user.vishalraj140706@gmail.com>
2025-04-18 16:21:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Schedule from './components/Schedule';
+import Events from './components/Events';
+import Sponsors from './components/Sponsors';
+import Prize from './components/Prize';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+     <div>
+      <Navbar />
+      <Home />
+     </div>
+  );
+}
+
+export default App;
